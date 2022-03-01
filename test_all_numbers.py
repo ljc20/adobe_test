ee9e5c661f58b71c27a99f509403ec1a46eba3b2
Luca Chammah <luca.chammah@icloud.com>
2022-03-01 15:33:36 +0000

    adobe commit 2

--- test_all_numbers.py
+++ test_all_numbers.py
@@ -1,21 +1,11 @@
 import urllib.request
 
-#weburl = urllib.request.urlopen('http://localhost:8080/romannumeral?query=50')
-#data = weburl.read()
-#print(data)
-#weburl = urllib.request.urlopen('http://localhost:8080/romannumeral?query=10')
-#data = weburl.read()
-#print(data)
-#i = 1
-#str(i)
-#weburl = urllib.request.urlopen('http://localhost:8080/romannumeral?query='+str(i))
-#data = weburl.read()
-#print(data)
 i = 1
 while(i < 256): # same concept as test_1_number.py but for all the numebrs in the range
     weburl = urllib.request.urlopen('http://localhost:8080/romannumeral?query='+str(i))
     data = weburl.read()
     print(data)
     i +=1
+
 
 
